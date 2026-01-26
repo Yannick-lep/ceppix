47560179b251bfbba254ad2d762452e8af525732
JulienTouchard <julien.touchard@free.fr>
2026-01-26 16:33:57 +0100

    crud main

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once("./inc/autoloader.php");
-var_dump(__DIR__."/controller/UserController.php");
-UserController::getUserByEmail();
-FilmController::getFilmsByCast();
-/* include_once("./public/templates/register.html.php"); */
+var_dump(__DIR__ . "/controller/UserController.php");
+/* UserController::getUserByEmail();
+FilmController::getFilmsByCast(); */
+include_once("./public/templates/register.html.php");
+
--- repository/MainRepository.php
+++ repository/MainRepository.php
@@ -1,16 +1,60 @@
 <?php
 
-class MainRepository {
-    // quelle table ?
-    // ...
-    public function getBy( ?array $value, ){
-        var_dump($value);
-        //$value ["user","email","azerty@azerty.com"]
-
-        //$req = $pdo->prepare("SELECT * FROM user WHERE email = 'azerty@azerty.com'");
+class MainRepository
+{
+    // CREATE
+    public function create(?array $values, $table)
+    {
         global $pdo;
-        $req = $pdo->prepare("SELECT * FROM ".$value[0]." WHERE ".$value[1]." = :valeur");
-        $req->execute([":valeur"=>$value[2]]);
-        return $req->fetchAll();    
+        $keys = "";
+        $keysAlias = "";
+        foreach ($values as $key => $value) {
+            $keys === "" ? $keys .= $key : $keys .= "," . $key;
+            $keysAlias === "" ? $keysAlias .= ":" . $key : $keysAlias .= "," . ":" . $key;
+        }
+        $req = $pdo->prepare("INSERT INTO $table ($keys) VALUES ($keysAlias)");
+        $req->execute($values);
+        return $req->fetchAll();
+    }
+    // READs
+    public function getBy(?array $value)
+    {
+        global $pdo;
+        $req = $pdo->prepare("SELECT * FROM " . $value[0] . " WHERE " . $value[1] . " = :valeur");
+        $req->execute([":valeur" => $value[2]]);
+        return $req->fetchAll();
+    }
+    public function getById($id, $table)
+    {
+        global $pdo;
+        $req = $pdo->prepare("SELECT * FROM $table WHERE id_$table = :id");
+        $req->execute([":id" => $id]);
+        return $req->fetch();
+    }
+    public function getByAll($table)
+    {
+        global $pdo;
+        $req = $pdo->prepare("SELECT * FROM $table");
+        $req->execute();
+        return $req->fetchAll();
+    }
+    // UPDATE
+    public function update($id, $values, $table)
+    {
+        global $pdo;
+        $updateValues = "";
+        foreach ($values as $key => $value) {
+            $updateValues === "" ? $updateValues .= $key." = :".$key : $updateValues .= ", ".$key." = :".$key;  
+        }
+        $req = $pdo->prepare("UPDATE $table SET $updateValues WHERE id_$table = $id");
+        $req->execute($values);
+        return $req->fetchAll();
+    }
+    // DELETE
+    public function delete($id, $table)
+    {
+        global $pdo;
+        $req = $pdo->prepare("DELETE FROM $table WHERE id_$table = $id");
+        $req->execute();
     }
 }
