71e14528c54fcb40547f07ec3746aeacaa1f7c29
JulienTouchard <julien.touchard@free.fr>
2026-01-27 16:57:34 +0100

    img

--- controller/FilmController.php
+++ controller/FilmController.php
@@ -8,7 +8,13 @@ class FilmController {
     }
     public static function getRandomFilms(){
         $filmRepository = new FilmRepository;
-        
-        return $filmRepository->getRandomFilms();
+        $randFilms = $filmRepository->getRandomFilms();
+        foreach ($randFilms as $key => $value) {
+            $imgurl = "./public/assets/posters/".$value['id_movies_full'].".jpg";
+            if(file_exists($imgurl)){
+                $randFilms[$key]['img']=$imgurl;
+            }
+        }
+        return $randFilms;
     }
 }
--- public/templates/previewFilms.html.php
+++ public/templates/previewFilms.html.php
@@ -1,18 +1,34 @@
-
 <style>
-    .preview-films{
+    *{
+        box-sizing: border-box;
+    }
+    .preview-films {
         display: flex;
         flex-wrap: wrap;
         justify-content: space-between;
+        padding: 1rem;
     }
-    .card-film{
+
+    .card-film {
+        padding: .5rem;
         width: 24%;
+        border-radius: 15px;
+        -webkit-box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.27);
+        box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.27);
+        margin-bottom: 1rem;
+    }
+    .card-film:last-child {
+        margin-right: auto;
+        margin-left: 1.5%;
+    }
+    .title{
+        font-weight: 900;
     }
 </style>
 <div class="preview-films">
     <?php foreach (FilmController::getRandomFilms() as $key => $value) { ?>
         <div class="card-film">
-            <div><img src="" alt=""></div>
+            <div><img src="<?= $value['img'] ?>" alt=""></div>
             <p class="title"><?= $value['title'] ?></p>
             <p class="plot"><?= $value['plot'] ?></p>
             <p class="year"><?= $value['year'] ?></p>
